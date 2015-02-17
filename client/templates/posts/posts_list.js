dc6596ceb1b6d2ea72585218dacd949f929428f9
ralphbolo <rbolo062@uottawa.ca>
2015-02-16 23:14:42 -0500

    chapter 3 templates

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,19 @@
+var postsData = [
+{
+		title: "Ralph Bolo",
+		url: "http://ralphbolo.me"
+	},
+	{
+		title: "NBA",
+		url: "http://nba.com"
+	}
+	,{
+		title: "twitter",
+		url: "http://twitter.com"
+	}
+];
+Template.postsList.helpers({
+	posts: postsData
+});
+	
+
