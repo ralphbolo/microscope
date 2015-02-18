d108fe65373f933939ea5222907e97fc787be2b8
ralphbolo <rbolo062@uottawa.ca>
2015-02-18 13:08:49 -0500

    chapter 10 comments

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,48 @@
 if (Posts.find().count() === 0){
-	Posts.insert({
+	var timeNow = new Date().getTime();
+
+	var ralphId = Meteor.users.insert({
+		profile: {name: 'Ralph Alfie'}
+	});
+	var ralph = Meteor.users.findOne(ralphId);
+
+	var mikeId = Meteor.users.insert({
+		profile: {name: 'Mike Jones'}
+	});
+	var mike = Meteor.users.findOne(mikeId);
+
+
+	var nbaId = Posts.insert({
 		title: "Ralph Bolo",
-		url: "http://ralphbolo.me"
+		userId: ralph._id,
+		author: ralph.profile.name,
+		url: "http://nba.com",
+		submitted: new Date(timeNow - 7 * 36000 * 1000),
+		commentsCount: 3
 	});
 
-	Posts.insert({
-		title: "twitter",
-		url: "http://twitter.com/bolonomicz"
+	Comments.insert({
+		postId: nbaId,
+		userId: ralph._id,
+		author: ralph.profile.name,
+		submitted: new Date(timeNow - 3 * 3600 * 1000),
+		body: "story number 3!"
 	});
 
-	Posts.insert({
-		title: "NBA",
-		url: "nba"
-	})
+	Comments.insert({
+		postId: nbaId,
+		userId: mike._id,
+		author: mike.profile.name,
+		submitted: new Date(timeNow - 5 * 3600 * 1000),
+		body: "amazing site!"
+	});
+
+	Comments.insert({
+		postId: nbaId,
+		userId: mike._id,
+		author: mike.profile.name,
+		submitted: new Date(timeNow - 12 * 3600 * 1000),
+		body: "look a penny!"
+	});
+
 }
